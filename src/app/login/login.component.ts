0c788ff7d309f5288436d65f875a2e9fbb514e5c
Anuj Tomar <anuj.tomar@knoldus.com>
2023-10-26 19:49:12 +0530

    Added animation & login page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, Renderer2  } from '@angular/core';
 import { faAt, faUser, faLock,faArrowUp } from '@fortawesome/free-solid-svg-icons'
 @Component({
   selector: 'app-login',
@@ -10,7 +10,21 @@ export class LoginComponent implements OnInit {
   passwordIcon = faLock
   userIcon = faUser
   arrowIcon = faArrowUp
-  constructor() { }
+  constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
+
+  ngAfterViewInit(): void {
+    const signUpButton = this.elementRef.nativeElement.querySelector('#signUp');
+    const signInButton = this.elementRef.nativeElement.querySelector('#signIn');
+    const container = this.elementRef.nativeElement.querySelector('#container');
+
+    signUpButton.addEventListener('click', () =>
+      this.renderer.addClass(container, 'right-panel-active')
+    );
+
+    signInButton.addEventListener('click', () =>
+      this.renderer.removeClass(container, 'right-panel-active')
+    );
+  }
 
   ngOnInit(): void {
   }
