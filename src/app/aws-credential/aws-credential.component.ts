8eb75b8bc5da09e28a8a1484c0cbc7e29374ae57
devansh-kapoor <devansh.kapoor@knoldus.com>
2023-10-26 18:43:52 +0530

    added aws,gcp and azure form page

---
+++ src/app/aws-credential/aws-credential.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-aws-credential',
+  templateUrl: './aws-credential.component.html',
+  styleUrls: ['./aws-credential.component.scss']
+})
+export class AwsCredentialComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
